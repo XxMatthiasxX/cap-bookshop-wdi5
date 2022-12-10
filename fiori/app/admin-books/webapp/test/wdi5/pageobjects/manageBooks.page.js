c9161eda206804efee90f38bbe8613418a83e4b1
Matthias Adams <matthias.adams@sap.com>
2022-12-10 12:45:46 +0100

    implement tests

--- fiori/app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ fiori/app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -3,7 +3,28 @@ module.exports = {
    * define actions for the page object
    */
 
+  iClickOnCreateNewBook: async () => {
+    await browser.asControl({
+    	selector: {
+            id: "books::BooksList--fe::table::Books::LineItem::StandardAction::Create",
+            interaction: {
+                    idSuffix: "BDI-content"
+            }
+    }}).press();
+  },
+
   /**
    * define assertions for the page object
    */
+
+  theBookListContains: async (sTitle) => {
+        await browser.asControl({
+                selector: {
+                controlType: "sap.m.Text",
+                viewId: "books::BooksList",
+                properties: {
+                        text: sTitle
+                }
+        }});
+    }
 }
--- fiori/app/admin-books/webapp/test/wdi5/specs/bookshop.test.js
+++ fiori/app/admin-books/webapp/test/wdi5/specs/bookshop.test.js
@@ -1,6 +1,19 @@
 const ManageBooksPage = require("../pageobjects/manageBooks.page");
 const NewBookPage = require("../pageobjects/newBook.page");
+const bookTitle = "How to Cook Pancakes";
 
 describe("Manage bookshop", () => {
+    it("create a new book", async () => {
+        await ManageBooksPage.iClickOnCreateNewBook();
+        await NewBookPage.iEnterTitle(bookTitle);
+        await NewBookPage.iSelectGenre();
+        await NewBookPage.iSelectAuthor();
+        await NewBookPage.iPressCreate();
+        await NewBookPage.iSeeEditButton();
+        await NewBookPage.iNavigateBack();
+    });
 
+    it("should check book is added", async () => {
+        await ManageBooksPage.theBookListContains(bookTitle);
+    });
 });
